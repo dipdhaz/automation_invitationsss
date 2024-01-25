b6e482179019852d5f1265566059511e4f7efe8e
dipdhaz <115815419+dipdhaz@users.noreply.github.com>
2024-01-25 19:19:25 +0800

    new changes

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use setasign\Fpdi\Fpdi;
 use App\Models\User;
+use Illuminate\Support\Facades\Storage;
 
 class PDFController extends Controller
 {
@@ -36,7 +37,7 @@ class PDFController extends Controller
             $fpdi->AddPage($size['orientation'], array($size['width'], $size['height']));
             $fpdi->useTemplate($template);
 
-            $fpdi->SetFont("Times", "", 10);
+            $fpdi->SetFont("Times", "", 8);
             $fpdi->SetTextColor(0, 0, 0);
             //Kondisi
 
@@ -45,6 +46,23 @@ class PDFController extends Controller
 
                 // Add user information to the PDF
                 // Add more text
+
+                // kolom dtpln
+                $fpdi->SetFont("Times", "",8);
+                $left2 = 138;
+                $top2 = 132.5;
+                $text2 = $user->no_dptln;
+                $fpdi->Text($left2, $top2, $text2);
+
+                // Add more text
+                $left2 = 138;
+                $top2 = 279;
+                $text2 = $user->no_dptln;
+                $fpdi->Text($left2, $top2, $text2);
+                 // kolom dtpln
+
+
+
                 $left2 = 113;
                 $top2 = 49;
                 $text2 = "08:00";
@@ -84,12 +102,6 @@ class PDFController extends Controller
                 $text6 = $user->alamat;
                 $fpdi->Text($left6, $top6, $text6);
 
-                // $left6 = 100;
-                // $top6 = 59;
-                // $text6 = $user->tps;
-                // $fpdi->Text($left6, $top6, $text6);
-
-
                 // Add user information to the PDF
 
 
@@ -98,12 +110,12 @@ class PDFController extends Controller
                 $text6 = $user->alamat;
                 $fpdi->Text($left6, $top6, $text6);
 
-                $left6 = 105;
+                $left6 = 98;
                 $top6 = 59;
                 if ($user->metode_pemilihan_id == 1) {
-                    $text6 = "TPS" . $user->metode_pemilihan;
+                    $text6 = "TPS" ."  ". $user->metode_pemilihan;
                 } else {
-                    $text6 = $user->metode_pemilihan;
+                    $text6 ="00" .$user->metode_pemilihan;
                 }
 
 
@@ -170,99 +182,131 @@ class PDFController extends Controller
                 $text7 = "2024";
                 $fpdi->Text($left7, $top7, $text7);
 
-
-
                 $left6 = 100;
                 $top6 = 212;
                 $text6 = $user->alamat;
                 $fpdi->Text($left6, $top6, $text6);
 
-                $left6 = 100;
+                $left6 = 98;
                 $top6 = 206;
-
                 if ($user->metode_pemilihan_id == 1) {
-                    $text6 = "TPS" . $user->metode_pemilihan;
+                    $text6 = "TPS" ."  ". $user->metode_pemilihan;
                 } else {
-                    $text6 = $user->metode_pemilihan;
+                    $text6 ="00" .$user->metode_pemilihan;
                 }
-
                 $fpdi->Text($left6, $top6, $text6);
 
-
-                $fpdi->SetFont("Times", "B", 12);
-                $left8 = 77.5;
+                $fpdi->SetFont("Times", "B", 8);
+                $left8 = 123;
                 $top8 = 29.5;
                 $text8 = "/";
                 $fpdi->Text($left8, $top8, $text8);
 
-                $left8 = 77.5;
+                $left8 = 119.5;
                 $top8 = 177;
                 $text8 = "/";
                 $fpdi->Text($left8, $top8, $text8);
 
-                $fpdi->SetFont("Times", "", 7);
-                $left = 52;
+
+                $fpdi->SetFont("Times", "", 8);
+                $left = 48;
                 $top = 28.5;
                 $text = $user->nama_lengkap; // Use the specific attribute of the user model
                 $fpdi->Text($left, $top, $text);
 
-                $left8 = 52;
+                $left8 = 48;
                 $top8 = 176;
                 $text8 = $user->nama_lengkap;
                 $fpdi->Text($left8, $top8, $text8);
-
 
                 $left6 = 125;
                 $top6 = 39;
                 $text6 = "FEBRUARI ";
                 $fpdi->Text($left6, $top6, $text6);
 
-
-
                 $left6 = 125;
                 $top6 = 186;
                 $text6 = "FEBRUARI ";
                 $fpdi->Text($left6, $top6, $text6);
 
-
-                $left8 = 137.5;
-                $top8 = 176;
-                $text8 = $user->nomor_paspor;
+                $left8 = 180;
+                $top8 = 176.6;
+                $text8 = $user->nomor_passpor;
                 $fpdi->Text($left8, $top8, $text8);
 
-                $left8 = 137.5;
+                $left8 = 181;
                 $top8 = 28.5;
-                $text8 = $user->nomor_paspor;
+                $text8 = $user->nomor_passpor;
                 $fpdi->Text($left8, $top8, $text8);
 
                 // Add more text
-                $fpdi->SetFont("Times", "B", 10);
-                $left2 = 45;
+                $fpdi->SetFont("Times", "B", 8);
+                $left2 = 46.3;
                 $top2 = 15;
                 $text2 = $user->no_dptln;
                 $fpdi->Text($left2, $top2, $text2);
 
                 // Add more text
-                $left2 = 45;
-                $top2 = 162.2;
+                $left2 = 46.3;
+                $top2 = 162.5;
                 $text2 = $user->no_dptln;
                 $fpdi->Text($left2, $top2, $text2);
+
+                // $left2 = 200;
+                // $top2 = 39;
+                // $text2 = $user->nama_lengkap;
+                // $fpdi->Text($left2, $top2, $text2);
+
+                // khusus kolom cowok
+                $left2 = 138;
+                $top2 = 126.5;
+                $text2 = $user->nama_lengkap;
+                $fpdi->Text($left2, $top2, $text2);
+
+                $left2 = 138;
+                $top2 = 274.5;
+                $text2 = $user->nama_lengkap;
+                $fpdi->Text($left2, $top2, $text2);
+
+
+                $left6 = 202;
+                $top6 = 275;
+                $text6 = "/";
+                $fpdi->Text($left6, $top6, $text6);
+
+                $left6 = 202;
+                $top6 = 126.5;
+                $text6 = "/";
+                $fpdi->Text($left6, $top6, $text6);
+                // khusus kolom cowok
+
+
             } else {
                 //JIKA PEREMPUAN
 
                 // Add user information to the PDF
+
                 // Add more text
+
+                $left6 = 140;
+                $top6 = 274;
+                $text6 = $user->nama_lengkap;
+                $fpdi->Text($left6, $top6, $text6);
+
+                $left6 = 140;
+                $top6 = 280;
+                $text6 = $user->no_dptln;
+                $fpdi->Text($left6, $top6, $text6);
+
                 $left2 = 113;
                 $top2 = 49;
                 $text2 = "08:00";
                 $fpdi->Text($left2, $top2, $text2);
 
-
                 $left3 = 167;
                 $top3 = 49;
                 $text3 = "MYT";
                 $fpdi->Text($left3, $top3, $text3);
-
 
                 $left3 = 138;
                 $top3 = 49;
@@ -275,7 +319,6 @@ class PDFController extends Controller
                 $text2 = "08:00";
                 $fpdi->Text($left2, $top2, $text2);
 
-
                 $left3 = 138;
                 $top3 = 44;
                 $text3 = "18:00";
@@ -296,12 +339,12 @@ class PDFController extends Controller
                 $text6 = $user->alamat;
                 $fpdi->Text($left6, $top6, $text6);
 
-                $left6 = 100;
+                $left6 = 98;
                 $top6 = 59;
                 if ($user->metode_pemilihan_id == 1) {
-                    $text6 = "TPS" . $user->metode_pemilihan;
+                    $text6 = "TPS" ."  ". $user->metode_pemilihan;
                 } else {
-                    $text6 = $user->metode_pemilihan;
+                    $text6 ="00" .$user->metode_pemilihan;
                 }
                 $fpdi->Text($left6, $top6, $text6);
 
@@ -309,7 +352,6 @@ class PDFController extends Controller
                 $top7 = 39;
                 $text7 = "2024";
                 $fpdi->Text($left7, $top7, $text7);
-
 
                 $left4 = 98;
                 $top4 = 39;
@@ -322,12 +364,10 @@ class PDFController extends Controller
                 $text2 = "08:00";
                 $fpdi->Text($left2, $top2, $text2);
 
-
                 $left3 = 167;
                 $top3 = 196;
                 $text3 = "MYT";
                 $fpdi->Text($left3, $top3, $text3);
-
 
                 $left3 = 138;
                 $top3 = 196;
@@ -351,83 +391,93 @@ class PDFController extends Controller
                 $text3 = "MYT";
                 $fpdi->Text($left3, $top3, $text3);
 
-
                 $left4 = 98;
                 $top4 = 186;
                 $text4 = "5-11";
                 $fpdi->Text($left4, $top4, $text4);
-
 
                 $left7 = 140;
                 $top7 = 186;
                 $text7 = "2024";
                 $fpdi->Text($left7, $top7, $text7);
 
-
-
                 $left6 = 100;
                 $top6 = 212;
                 $text6 = $user->alamat;
                 $fpdi->Text($left6, $top6, $text6);
 
-                $left6 = 100;
+                $left6 = 98;
                 $top6 = 206;
                 if ($user->metode_pemilihan_id == 1) {
-                    $text6 = "TPS" . $user->metode_pemilihan;
+                    $text6 = "TPS" ."  ". $user->metode_pemilihan;
                 } else {
-                    $text6 = $user->metode_pemilihan;
+                    $text6 ="00" .$user->metode_pemilihan;
                 }
                 $fpdi->Text($left6, $top6, $text6);
 
-
-                $fpdi->SetFont("Times", "B", 12);
-                $left8 = 73;
+                $fpdi->SetFont("Times", "B", 8);
+                $left8 = 91;
                 $top8 = 29.5;
                 $text8 = "/";
                 $fpdi->Text($left8, $top8, $text8);
 
-                $left8 = 73;
-                $top8 = 177;
+                $left8 = 91;
+                $top8 = 177.5;
                 $text8 = "/";
                 $fpdi->Text($left8, $top8, $text8);
 
-                $fpdi->SetFont("Times", "", 7);
-                $left = 52;
+                // khusus kolom
+
+                $left = 198;
+                $top = 128;
+                $text = "/"; // Use the specific attribute of the user model
+                $fpdi->Text($left, $top, $text);
+
+                $left6 = 198;
+                $top6 = 275;
+                $text6 = "/";
+                $fpdi->Text($left6, $top6, $text6);
+
+
+                // khusus kolom
+                $fpdi->SetFont("Times", "", 8);
+                $left = 48;
                 $top = 28.5;
                 $text = $user->nama_lengkap; // Use the specific attribute of the user model
                 $fpdi->Text($left, $top, $text);
 
-                $left8 = 52;
+                $left8 = 48;
                 $top8 = 176;
                 $text8 = $user->nama_lengkap;
                 $fpdi->Text($left8, $top8, $text8);
 
-
-                $left6 = 125;
-                $top6 = 39;
-                $text6 = "FEBRUARI ";
-                $fpdi->Text($left6, $top6, $text6);
-
-
-
-                $left6 = 125;
-                $top6 = 186;
-                $text6 = "FEBRUARI ";
-                $fpdi->Text($left6, $top6, $text6);
+                // ini khusus form di bawah
+                $fpdi->SetFont("Times", "", 8);
+                $left = 140;
+                $top = 126.5;
+                $text = $user->nama_lengkap; // Use the specific attribute of the user model
+                $fpdi->Text($left, $top, $text);
 
 
-                $left8 = 137.5;
-                $top8 = 176;
-                $text8 = $user->nomor_paspor;
+                $fpdi->SetFont("Times", "", 8);
+                $left = 140;
+                $top = 133;
+                $text = $user->no_dptln; // Use the specific attribute of the user model
+                $fpdi->Text($left, $top, $text);
+
+                $fpdi->SetFont("Times", "", 8);
+                $left8 = 180;
+                $top8 = 176.5;
+                $text8 = $user->nomor_passpor;
                 $fpdi->Text($left8, $top8, $text8);
 
-                $left8 = 137.5;
+                $left8 = 181;
                 $top8 = 28.5;
-                $text8 = $user->nomor_paspor;
+                $text8 = $user->nomor_passpor;
                 $fpdi->Text($left8, $top8, $text8);
 
                 // Add more text
-                $fpdi->SetFont("Times", "B", 10);
+                $fpdi->SetFont("Times", "B", 8);
                 $left2 = 45;
                 $top2 = 15;
                 $text2 = $user->no_dptln;
@@ -438,20 +488,29 @@ class PDFController extends Controller
                 $top2 = 162.2;
                 $text2 = $user->no_dptln;
                 $fpdi->Text($left2, $top2, $text2);
+
+                $fpdi->SetFont("Times", "", 8);
+                $left6 = 125;
+                $top6 = 39;
+                $text6 = "FEBRUARI ";
+                $fpdi->Text($left6, $top6, $text6);
+
+                $left6 = 125;
+                $top6 = 186;
+                $text6 = "FEBRUARI ";
+                $fpdi->Text($left6, $top6, $text6);
             }
 
+            // $uniqueOutputPath = $outputFilePath . '_' . $user->nama_lengkap . '.pdf';
+            // $fpdi->Output($uniqueOutputPath, 'F' );
+            // Build a unique file name based on user attributes
+        $uniqueOutputPath = "{$user->nama_lengkap}_{$i}.pdf";
 
-
-
-
-
-
-
-
-            $uniqueOutputPath = $outputFilePath . '_' . $text . '.pdf';
-            $fpdi->Output($uniqueOutputPath, 'F');
+        // Output the PDF content to the storage disk
+        $fpdi->Output('F', storage_path("app/public/{$uniqueOutputPath}"));
         }
 
-        return response()->file($uniqueOutputPath);
+        // return response()->file($uniqueOutputPath);
+        return response()->download(storage_path("app/public/{$uniqueOutputPath}"));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+    <title>Undangan PPLNJB </title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('path/to/your/icomoon/style.css') }}">
+
 
     <style>
         body {
@@ -23,7 +25,15 @@
             align-items: center;
             justify-content: center;
             min-height: 100vh;
-            overflow: auto;
+            overflow: hidden;
+            top: 150;
+        }
+
+        @media (min-width: 1200px) {
+            body {
+                background-size: 100% 100%;
+                /* Adjust background size for larger screens */
+            }
         }
 
         .container {
@@ -38,31 +48,46 @@
 
         .logo {
             max-width: 150px;
-            /* Responsive logo size */
+            top: 200;
+
         }
 
         .search-bar-container {
-            position: relative;
+            /* position: relative; */
+            /* width: 100%; */
+            /* Ensure the search bar container takes the full width */
+            padding: 20px;
+            /* Add padding to the search bar container for spacing */
+            box-sizing: border-box;
+            /* Include padding and border in the total width/height */
         }
 
         .search-form {
             display: flex;
             align-items: center;
             position: relative;
+            width: 100%;
+            /* Make the search bar take the full width of its container */
+            max-width: 650px;
+            /* Adjust as needed */
         }
 
         .search-bar {
-            width: 650px; /* Set the width for the normal screen size */
-            max-width: 90%; /* Maximum width to maintain responsiveness */
+            width: 650px;
+
+            /* Set the width for the normal screen size */
+            max-width: 90%;
+            /* Maximum width to maintain responsiveness */
             height: 35px;
             padding: 10px;
             border: none;
             border-radius: 10px;
             font-size: 16px;
-            background-size: cover; /* Updated background size */
-            background-position: 5% 50%;
+            background-size: cover;
+            /* Updated background size */
+            background-position: 100% 100%;
             background-repeat: no-repeat;
-            text-align: center;
+            /* text-align: center; */
             color: #999;
         }
 
@@ -73,16 +98,18 @@
             border: none;
             cursor: pointer;
             position: absolute;
-            left: 10px;
+            left: 8px;
+
         }
 
         .container {
-            font-size: 33px;
+            font-size: 28px;
         }
 
         @media screen and (max-width: 600px) {
             body {
-                background-size: cover;
+                /* background-size: cover; */
+                background-attachment: scroll;
             }
 
             .container {
@@ -105,54 +132,55 @@
     <div class="container">
         <!-- Your existing content goes here -->
         <img src="{{ asset('images/1.png') }}" class="logo" alt="Logo">
-        <p>Dapatkan undangan pemilihan umum dengan menginput nomor paspor/NIK</p>
+        <p>Dapatkan Undangan Pemilihan Umum Dengan Menginput Nomor Passport Atau NIK</p>
 
         <div class="search-bar-container">
             <form action="{{ route('user.search') }}" method="GET" class="search-form">
                 <input type="text" class="search-bar" placeholder="Enter NIK or Passport Number" name="query"
                     required>
                 <button type="submit" class="search-button">
-                    <img src="{{ asset('images/cari.jpg') }}" alt="Search">
+                    @svg('icomoon-search')
+
                 </button>
             </form>
         </div>
     </div>
-   <!-- Your other HTML content -->
+    <!-- Your other HTML content -->
 
-<!-- Include SweetAlert library -->
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <!-- Include SweetAlert library -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
-<!-- Check for the flashed session variable and display SweetAlert -->
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        @if(session('error'))
-            Swal.fire({
-                icon: 'error',
-                title: 'Error',
-                text: '{{ session('error') }}',
-            });
-        @endif
-
-        // Check for internet connectivity
-        function checkInternetConnection() {
-            if (navigator.onLine) {
-                // The device is online
-            } else {
-                // The device is offline, show a SweetAlert notification
+    <!-- Check for the flashed session variable and display SweetAlert -->
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            @if (session('error'))
                 Swal.fire({
                     icon: 'error',
-                    title: 'No Internet Connection',
-                    text: 'Please check your internet connection and try again.',
+                    title: 'Error',
+                    text: '{{ session('error') }}',
                 });
-            }
-        }
+            @endif
 
-        // Call the function initially and add an event listener for future changes
-        checkInternetConnection();
-        window.addEventListener('online', checkInternetConnection);
-        window.addEventListener('offline', checkInternetConnection);
-    });
-</script>
+            // Check for internet connectivity
+            function checkInternetConnection() {
+                if (navigator.onLine) {
+                    // The device is online
+                } else {
+                    // The device is offline, show a SweetAlert notification
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'No Internet Connection',
+                        text: 'Please check your internet connection and try again.',
+                    });
+                }
+            }
+
+            // Call the function initially and add an event listener for future changes
+            checkInternetConnection();
+            window.addEventListener('online', checkInternetConnection);
+            window.addEventListener('offline', checkInternetConnection);
+        });
+    </script>
 </body>
 
 </html>
